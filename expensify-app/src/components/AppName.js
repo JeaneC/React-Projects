8fa7da623ab0a068c216e627c237aa3d17169323
Jeane Carlos <volbohel@gmail.com>
2017-10-04 09:18:12 -0400

    Expensify Boilerplate + ReactRouter Integration

---
+++ expensify-app/src/components/AppName.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class AppName extends React.Component {
+  state = {
+    options: undefined
+  }
+
+  render() {
+
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    )
+  }
+}
+
+export default AppName
